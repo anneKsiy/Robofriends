c1992f498d77601c5a4f48adb8b518b18615d7ea
Abdullah Adam <abuidreesadam@gmail.com>
2020-08-07 14:36:19 +0200

    Updates

---
+++ precache-manifest.4b628a2208aeb916dc3325d8c3e99ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde5be6ef0656c02864544673fb5b2e9",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4cc9f92d5f739a536bf0",
+    "url": "/Robofriends/static/css/main.6b1d1509.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc9f92d5f739a536bf0",
+    "url": "/Robofriends/static/js/main.d47b8766.chunk.js"
+  },
+  {
+    "revision": "a6103a62c81f6271b765",
+    "url": "/Robofriends/static/js/runtime-main.79cc7977.js"
+  }
+]);
